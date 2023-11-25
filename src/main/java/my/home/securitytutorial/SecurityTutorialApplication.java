0ab727e870009e5ac048983f14fa6b3d3ba2705f
IOSYS <118172294+iosys-hub@users.noreply.github.com>
2023-11-25 19:07:08 +0900

    등록

---
+++ src/main/java/my/home/securitytutorial/SecurityTutorialApplication.java
@@ -0,0 +1,13 @@
+package my.home.securitytutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/home/securitytutorial/SecurityTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package my.home.securitytutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
